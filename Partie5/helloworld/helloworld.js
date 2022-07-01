a9ba6592e9cbf63026e13d4c22463705d809bd2d
TICHANE Jean-Michel <107214400+TICHANE-JM@users.noreply.github.com>
2022-07-01 12:19:01 +0200

    Create helloworld.js

---
+++ Partie5/helloworld/helloworld.js
@@ -0,0 +1,15 @@
+var http = require("http")
+
+http.createServer(function (request, response) {
+
+   // Envoyer l'en-tête HTTP
+   // Statut HTTP : 200 : OK
+   // Type de contenu : texte/brut
+   response.writeHead(200, {'Content-Type': 'text/plain'})
+
+   // Envoyer le corps de la réponse sous la forme "Hello World"
+   response.end('Hello World\n')
+}).listen(3000)
+
+// La console imprimera le message
+console.log('Server running')
